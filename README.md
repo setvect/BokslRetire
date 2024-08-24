05f70c2eef24bd4d661976df73a9d7a9a738d75e
setvect <setvect@naver.com>
2024-08-24 13:45:27 +0900

    입력 폼 작업

--- README.md
+++ README.md
@@ -1,46 +1,35 @@
-# Getting Started with Create React App
+# 복슬은퇴 계산기
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## 1. 주요기능
 
-## Available Scripts
+- 내 자산, 저축금액, 투자 수익률을 통해 은퇴후 사용가능한 지출 계산
 
-In the project directory, you can run:
+## 2. 실행 및 빌드
 
-### `npm start`
+### 2.1. 초기셋팅
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+```shell
+$ npm install
+```
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+### 2.2. 실행 방법
 
-### `npm test`
+```shell
+$ npm start
+```
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+### 2.3. 빌드 방법
 
-### `npm run build`
+```shell
+$ npm run build
+```
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+- 빌드 결과는 `` 폴더에 생성됨
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+## 3. 주요화면
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+## 4. 개발환경
 
-### `npm run eject`
+### 4.1. 주요 프레임워크 및 라이브러리
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+- [Material-UI](https://mui.com/)
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,217 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  Box,
+  CssBaseline,
+  createTheme,
+  ThemeProvider,
+  TextField,
+  InputAdornment,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  FormHelperText,
+  SelectChangeEvent,
+  Button,
+} from "@mui/material";
+import PetsIcon from "@mui/icons-material/Pets";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          fontFamily: '"Pretendard Variable", "Pretendard", "NanumGothic", "nanum-square-round-otf", serif',
+          WebkitFontSmoothing: "antialiased",
+          MozOsxFontSmoothing: "grayscale",
+        },
+      },
+    },
+  },
+});
 
 function App() {
+  const [netWorth, setNetWorth] = React.useState("");
+  const [annualSavings, setAnnualSavings] = React.useState("");
+  const [savingsGrowthRate, setSavingsGrowthRate] = React.useState("");
+  const [targetReturnRate, setTargetReturnRate] = React.useState("");
+  const [annualInflationRate, setAnnualInflationRate] = React.useState("");
+  const [expectedRetirementAge, setExpectedRetirementAge] = React.useState<string>("");
+
+  const handleExpectedRetirementAgeChange = (event: SelectChangeEvent<string>) => {
+    console.log("###########", event.target.value);
+    setExpectedRetirementAge(event.target.value);
+  };
+  const formatNumber = (value: string) => {
+    return value.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  };
+  const handleNetWorthChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let value = event.target.value.replace(/,/g, "");
+    if (value === "-" || !isNaN(Number(value))) {
+      setNetWorth(formatNumber(value));
+    }
+  };
+  const handleAnnualSavingsChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let value = event.target.value.replace(/,/g, "");
+    if (value === "-" || !isNaN(Number(value))) {
+      setAnnualSavings(formatNumber(value));
+    }
+  };
+  const handleSavingsGrowthRateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let value = event.target.value.replace(/,/g, "");
+    if (value === "-" || !isNaN(Number(value))) {
+      setSavingsGrowthRate(formatNumber(value));
+    }
+  };
+  const handleTargetReturnRateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let value = event.target.value.replace(/,/g, "");
+    if (value === "-" || !isNaN(Number(value))) {
+      setTargetReturnRate(formatNumber(value));
+    }
+  };
+
+  const handleAnnualInflationRateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let value = event.target.value.replace(/,/g, "");
+    if (value === "-" || !isNaN(Number(value))) {
+      setAnnualInflationRate(formatNumber(value));
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <AppBar position="fixed">
+        <Toolbar
+          sx={{
+            minHeight: "50px !important",
+            height: "50px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <PetsIcon sx={{ color: "#ffee77", marginBottom: "3px", marginRight: "5px" }} />
+          <Typography variant="h6">복슬은퇴 계산기</Typography>
+        </Toolbar>
+      </AppBar>
+      <Box sx={{ mt: "55px" }}>
+        <Container maxWidth={false}>
+          <div style={{ display: "flex", alignItems: "center", width: "100%", gap: "16px" }}>
+            <TextField
+              label="순자산"
+              type="text"
+              sx={{ m: 1, flex: 1, width: "25ch", input: { textAlign: "right" } }}
+              value={netWorth}
+              onChange={handleNetWorthChange}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="start" sx={{ marginLeft: "5px" }}>
+                    만원
+                  </InputAdornment>
+                ),
+                inputProps: { maxLength: 10 },
+                style: { textAlign: "right" },
+              }}
+              variant="standard"
+            />
+            <TextField
+              label="한해 예상 저축금액"
+              type="text"
+              sx={{ m: 1, flex: 1, width: "25ch", input: { textAlign: "right" } }}
+              value={annualSavings}
+              onChange={handleAnnualSavingsChange}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="start" sx={{ marginLeft: "5px" }}>
+                    만원
+                  </InputAdornment>
+                ),
+                inputProps: { maxLength: 10 },
+                style: { textAlign: "right" },
+              }}
+              variant="standard"
+            />
+            <TextField
+              label="저축 증가률"
+              type="text"
+              sx={{ m: 1, flex: 1, width: "25ch", input: { textAlign: "right" } }}
+              value={savingsGrowthRate}
+              onChange={handleSavingsGrowthRateChange}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="start" sx={{ marginLeft: "5px" }}>
+                    %
+                  </InputAdornment>
+                ),
+                inputProps: { maxLength: 5 },
+                style: { textAlign: "right" },
+              }}
+              variant="standard"
+            />
+            <TextField
+              label="목표 수익률"
+              type="text"
+              sx={{ m: 1, flex: 1, width: "25ch", input: { textAlign: "right" } }}
+              value={targetReturnRate}
+              onChange={handleTargetReturnRateChange}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="start" sx={{ marginLeft: "5px" }}>
+                    %
+                  </InputAdornment>
+                ),
+                inputProps: { maxLength: 5 },
+                style: { textAlign: "right" },
+              }}
+              variant="standard"
+            />
+            <TextField
+              label="연평균 물가 상승률"
+              type="text"
+              sx={{ m: 1, flex: 1, width: "25ch", input: { textAlign: "right" } }}
+              value={annualInflationRate}
+              onChange={handleAnnualInflationRateChange}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="start" sx={{ marginLeft: "5px" }}>
+                    %
+                  </InputAdornment>
+                ),
+                inputProps: { maxLength: 5 },
+                style: { textAlign: "right" },
+              }}
+              variant="standard"
+            />
+            <FormControl sx={{ m: 1, flex: 1, minWidth: 250 }}>
+              <InputLabel id="demo-simple-select-helper-label">예상(희망) 은퇴 시기</InputLabel>
+              <Select
+                labelId="demo-simple-select-helper-label"
+                id="demo-simple-select-helper"
+                value={expectedRetirementAge}
+                onChange={handleExpectedRetirementAgeChange}
+                variant="standard"
+              >
+                <MenuItem value="">
+                  <em>-- 선택해주세요 --</em>
+                </MenuItem>
+                {[...Array(51)].map((_, index) => (
+                  <MenuItem key={index} value={index}>
+                    {index === 0 ? "지금당장" : `${index}년후`}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <Button variant="contained" color="success" sx={{ m: 2, flex: 1 }}>
+              결과확인
+            </Button>
+          </div>
+        </Container>
+      </Box>
+    </ThemeProvider>
   );
 }
 
