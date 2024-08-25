e3d193b8b72c0d99440146604c991038ab66c835
setvect <setvect@naver.com>
2024-08-25 12:27:13 +0900

    리포트 영역분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
-import React from "react";
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Container,
-  Box,
-  CssBaseline,
-  createTheme,
-  ThemeProvider,
-  TableContainer,
-  Table,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableBody,
-} from "@mui/material";
-import Paper from "@mui/material/Paper";
 import PetsIcon from "@mui/icons-material/Pets";
-import ConditionForm, { ReportCondition } from "./components/ConditionForm";
-import _ from "lodash";
+import { AppBar, Box, Container, createTheme, CssBaseline, ThemeProvider, Toolbar, Typography } from "@mui/material";
+import React from "react";
 import "./App.css";
+import ConditionForm, { ReportCondition } from "./components/ConditionForm";
+import Report from "./components/Report";
 
 const darkTheme = createTheme({
   palette: {
@@ -38,18 +22,6 @@ const darkTheme = createTheme({
   },
 });
 
-type RetirementCalculatorYear = {
-  year: number;
-  startAmount: number;
-  savingsAmount: number;
-  cumulativeInflationRate: number;
-  investmentIncome: number;
-  totalAssets: number;
-  livingExpenses: number;
-  remainingAssets: number;
-  remainingAssetsPresentValue: number;
-};
-
 function App() {
   const [condition, setCondition] = React.useState<ReportCondition>({
     netWorth: "",
@@ -60,18 +32,6 @@ function App() {
     expectedRetirementAge: "",
     retireSpend: "",
   });
-
-  const rows: RetirementCalculatorYear[] = _.range(1, 101).map((idx) => ({
-    year: 2023 + idx,
-    startAmount: 9,
-    savingsAmount: 10,
-    cumulativeInflationRate: 11,
-    investmentIncome: 12,
-    totalAssets: 13,
-    livingExpenses: 14,
-    remainingAssets: 15,
-    remainingAssetsPresentValue: 16,
-  }));
 
   return (
     <ThemeProvider theme={darkTheme}>
@@ -85,69 +45,7 @@ function App() {
       <Box sx={{ mt: "55px" }}>
         <Container maxWidth={false}>
           <ConditionForm condition={condition} setCondition={setCondition} />
-          <div>
-            <Typography variant="h6" component="h2">
-              결과
-            </Typography>
-            <TableContainer component={Paper} sx={{ maxHeight: "calc(100vh - 170px)" }} className="custom-scrollbar">
-              <Table
-                stickyHeader
-                sx={{
-                  tableLayout: "fixed",
-                  "& .MuiTableCell-root": {
-                    border: "1px solid #555",
-                  },
-                  "& .MuiTableHead-root .MuiTableCell-root": {
-                    fontWeight: "bold",
-                    backgroundColor: "background.paper",
-                    zIndex: 1,
-                  },
-                }}
-              >
-                <TableHead>
-                  <TableRow>
-                    <TableCell align="center">년도</TableCell>
-                    <TableCell align="center">시작금액</TableCell>
-                    <TableCell align="center">저축금액</TableCell>
-                    <TableCell align="center">투자소득</TableCell>
-                    <TableCell align="center">누적 자산</TableCell>
-                    <TableCell align="center">누적 물가 상승률</TableCell>
-                    <TableCell align="center">생활비</TableCell>
-                    <TableCell align="center">남은 자산</TableCell>
-                    <TableCell align="center">현재가치 환산</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {rows.map((row, index) => (
-                    <TableRow
-                      key={index}
-                      sx={
-                        (index + 1) % 5 === 0
-                          ? {
-                              "& .MuiTableCell-root": {
-                                borderBottom: "1px solid #fff",
-                              },
-                            }
-                          : {}
-                      }
-                    >
-                      <TableCell component="th" scope="row">
-                        {row.year}년 (올해)
-                      </TableCell>
-                      <TableCell align="right">{row.startAmount}만원</TableCell>
-                      <TableCell align="right">{row.savingsAmount}만원</TableCell>
-                      <TableCell align="right">{row.investmentIncome}만원</TableCell>
-                      <TableCell align="right">{row.totalAssets}만원</TableCell>
-                      <TableCell align="right">{row.cumulativeInflationRate}%</TableCell>
-                      <TableCell align="right">{row.livingExpenses}만원</TableCell>
-                      <TableCell align="right">{row.remainingAssets}만원</TableCell>
-                      <TableCell align="right">{row.remainingAssetsPresentValue}만원</TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </div>
+          <Report />
         </Container>
       </Box>
     </ThemeProvider>
---
+++ src/components/Report.tsx
@@ -0,0 +1,96 @@
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import Paper from "@mui/material/Paper";
+import _ from "lodash";
+
+type RetirementCalculatorYear = {
+  year: number;
+  startAmount: number;
+  savingsAmount: number;
+  cumulativeInflationRate: number;
+  investmentIncome: number;
+  totalAssets: number;
+  livingExpenses: number;
+  remainingAssets: number;
+  remainingAssetsPresentValue: number;
+};
+
+function Report() {
+  const rows: RetirementCalculatorYear[] = _.range(1, 101).map((idx) => ({
+    year: 2023 + idx,
+    startAmount: 9,
+    savingsAmount: 10,
+    cumulativeInflationRate: 11,
+    investmentIncome: 12,
+    totalAssets: 13,
+    livingExpenses: 14,
+    remainingAssets: 15,
+    remainingAssetsPresentValue: 16,
+  }));
+
+  const getBackgroundColor = (index: number) => {
+    const group = Math.floor(index / 5);
+    switch (group % 2) {
+      case 0:
+        return "#333";
+      case 1:
+        return "inherit";
+    }
+  };
+
+  return (
+    <div>
+      <Typography variant="h6" component="h2">
+        결과
+      </Typography>
+      <TableContainer component={Paper} sx={{ maxHeight: "calc(100vh - 170px)" }} className="custom-scrollbar">
+        <Table
+          stickyHeader
+          sx={{
+            tableLayout: "fixed",
+            "& .MuiTableCell-root": {
+              border: "1px solid #555",
+            },
+            "& .MuiTableHead-root .MuiTableCell-root": {
+              fontWeight: "bold",
+              backgroundColor: "background.paper",
+              zIndex: 1,
+            },
+          }}
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">년도</TableCell>
+              <TableCell align="center">시작금액</TableCell>
+              <TableCell align="center">저축금액</TableCell>
+              <TableCell align="center">투자소득</TableCell>
+              <TableCell align="center">누적 자산</TableCell>
+              <TableCell align="center">누적 물가 상승률</TableCell>
+              <TableCell align="center">생활비</TableCell>
+              <TableCell align="center">남은 자산</TableCell>
+              <TableCell align="center">현재가치 환산</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row, index) => (
+              <TableRow key={index} sx={{ backgroundColor: getBackgroundColor(index) }}>
+                <TableCell component="th" scope="row">
+                  {row.year}년 (올해)
+                </TableCell>
+                <TableCell align="right">{row.startAmount}만원</TableCell>
+                <TableCell align="right">{row.savingsAmount}만원</TableCell>
+                <TableCell align="right">{row.investmentIncome}만원</TableCell>
+                <TableCell align="right">{row.totalAssets}만원</TableCell>
+                <TableCell align="right">{row.cumulativeInflationRate}%</TableCell>
+                <TableCell align="right">{row.livingExpenses}만원</TableCell>
+                <TableCell align="right">{row.remainingAssets}만원</TableCell>
+                <TableCell align="right">{row.remainingAssetsPresentValue}만원</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+}
+
+export default Report;
